194bea569eaaa183e33fea4d5f8dcf102a3bc0cb
windsonleo <windsor_melo@hotmail.com>
2022-10-05 00:21:25 -0300

    Add files via upload

---
+++ addservico.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="CryptoDash admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities with bitcoin dashboard.">
+    <meta name="keywords" content="admin template, CryptoDash Cryptocurrency Dashboard Template, dashboard template, flat admin template, responsive admin template, web app, crypto dashboard, bitcoin dashboard">
+    <meta name="author" content="ThemeSelection">
+    <title>Adicionar Servico</title>
+    <link rel="apple-touch-icon" href="../images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../images/ico/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,700,700i|Comfortaa:300,400,500,700" rel="stylesheet">
+    <!-- BEGIN VENDOR CSS-->
+    <link rel="stylesheet" type="text/css" href="../css/vendors.css">
+    <link rel="stylesheet" type="text/css" href="../vendors/css/forms/toggle/switchery.min.css">
+    <link rel="stylesheet" type="text/css" href="../vendors/css/charts/chartist.css">
+    <link rel="stylesheet" type="text/css" href="../vendors/css/charts/chartist-plugin-tooltip.css">
+    <!-- END VENDOR CSS-->
+    <!-- BEGIN MODERN CSS-->
+    <link rel="stylesheet" type="text/css" href="../css/app.css">
+    <!-- END MODERN CSS-->
+    <!-- BEGIN Page Level CSS-->
+    <link rel="stylesheet" type="text/css" href="../css/core/menu/menu-types/vertical-compact-menu.css">
+    <link rel="stylesheet" type="text/css" href="../vendors/css/cryptocoins/cryptocoins.css">
+    <link rel="stylesheet" type="text/css" href="../css/pages/timeline.css">
+    <link rel="stylesheet" type="text/css" href="../css/pages/dashboard-ico.css">
+    <!-- END Page Level CSS-->
+    <!-- BEGIN Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+    <!-- END Custom CSS-->
+  </head>
+  <body class="vertical-layout vertical-compact-menu 2-columns   menu-expanded fixed-navbar" data-open="click" data-menu="vertical-compact-menu" data-col="2-columns">
+
+    <!-- fixed-top-->
+    <nav class="header-navbar navbar-expand-md navbar navbar-with-menu navbar-without-dd-arrow fixed-top navbar-light navbar-bg-color">
+      <div class="navbar-wrapper">
+        <div class="navbar-header d-md-none">
+          <ul class="nav navbar-nav flex-row">
+            <li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu font-large-1"></i></a></li>
+            <li class="nav-item d-md-none"><a class="navbar-brand" href="index.php"><img class="brand-logo d-none d-md-block" alt="CryptoDash admin logo" src="../images/logo/logo.png"><img class="brand-logo d-sm-block d-md-none" alt="CryptoDash admin logo sm" src="../images/logo/logo-sm.png"></a></li>
+            <li class="nav-item d-md-none"><a class="nav-link open-navbar-container" data-toggle="collapse" data-target="#navbar-mobile"><i class="la la-ellipsis-v">   </i></a></li>
+          </ul>
+        </div>
+        <div class="navbar-container">
+          <div class="collapse navbar-collapse" id="navbar-mobile">
+            <ul class="nav navbar-nav mr-auto float-left">
+              <li class="nav-item d-none d-md-block"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu">         </i></a></li>
+              <li class="nav-item nav-search"><a class="nav-link nav-link-search" href="#"><i class="ficon ft-search"></i></a>
+                <div class="search-input">
+                  <input class="input" type="text" placeholder="Pesquise...">
+                </div>
+              </li>
+            </ul>
+            <ul class="nav navbar-nav float-right">         
+    
+              <li class="dropdown dropdown-notification nav-item"><a class="nav-link nav-link-label" href="wallet.html"><i class="ficon icon-wallet"></i></a></li>
+              <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">             <span class="avatar avatar-online"><img src="../images/portrait/small/avatar-s-1.png" alt="avatar"></span><span class="mr-1">CIC<span class="user-name text-bold-700">3,458.88</span></span></a>
+                <div class="dropdown-menu dropdown-menu-right">             <a class="dropdown-item" href="account-profile.html"><i class="ft-award"></i>John Doe</a>
+                  <div class="dropdown-divider"></div><a class="dropdown-item" href="account-profile.html"><i class="ft-user"></i> Profile</a><a class="dropdown-item" href="wallet.html"><i class="icon-wallet"></i> My Wallet</a><a class="dropdown-item" href="transactions.html"><i class="ft-check-square"></i> Transactions              </a>
+                  <div class="dropdown-divider"></div><a class="dropdown-item" href="account-login.html"><i class="ft-power"></i> Logout</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <!-- ////////////////////////////////////////////////////////////////////////////-->
+
+
+    <div class="main-menu menu-fixed menu-dark menu-bg-default rounded menu-accordion menu-shadow">
+      <div class="main-menu-content"><a class="navigation-brand d-none d-md-block d-lg-block d-xl-block" href="index.php"><img class="brand-logo" alt="CryptoDash admin logo" src="../images/logo/logo.png"/></a>
+        <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+          <li class="nav-item"><a href="index.php"><i class="icon-grid"></i><span class="menu-title" data-i18n="">Dashboard</span></a>
+          </li>
+          <li class="nav-item"><a href="consultass.php"><i class="icon-support"></i><span class="menu-title" data-i18n="">Consultas</span></a>
+          </li>
+          <li class=" active"><a href="pacientess.php"><i class="icon-user-following"></i><span class="menu-title" data-i18n="">Pacientes</span></a>
+          </li>
+          <li class=" nav-item"><a href="psicologoss.php"><i class="icon-user"></i><span class="menu-title" data-i18n="">Psicologos</span></a>
+          </li>
+
+                              <li class="active"><a href="servicos.php"><i class="la la-dashcube"></i><span class="menu-title" data-i18n="">Serviços</span></a>
+          </li>
+
+          <li class=" nav-item"><a href="usuarios.php"><i class="la la-users"></i><span class="menu-title" data-i18n="">Usuarios</span></a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+
+    <div class="app-content content">
+      <div class="content-wrapper">
+        <div class="content-header row">
+
+        <div class="content-header-left col-md-8 col-12 mb-2 breadcrumb-new">
+            <h3 class="content-header-title mb-0 d-inline-block">Adicionar Serviços</h3>
+            <div class="row breadcrumbs-top d-inline-block">
+              <div class="breadcrumb-wrapper col-12">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="servicos.php">Serviços</a>
+                  </li>
+                  <li class="breadcrumb-item active">Adicionar Serviços
+                  </li>
+                </ol>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+        <div class="content-body"><!-- ICO Token &  Distribution-->
+
+<div class="row">
+    <div class="col-12 col-md-8">
+        <!-- User Profile -->
+        <section class="card">
+            <div class="card-content">
+                <div class="card-body">
+                    <div class="col-12">
+                        <div class="row">
+                            <div class="col-md-2 col-12">
+                                <img src="../images/portrait/medium/avatar-m-1.png" class="rounded-circle height-100" alt="Card image" />
+                            </div>
+                            <div class="col-md-10 col-12">
+                                <div class="row">
+                                    <div class="col-12 col-md-4">
+                                        <p class="text-bold-700 text-uppercase mb-0">Consultas</p>
+                                        <p class="mb-0">12/14</p>
+                                    </div>
+                                    <div class="col-12 col-md-4">
+                                        <p class="text-bold-700 text-uppercase mb-0">Ultimo login</p>
+                                        <p class="mb-0">2018-04-30 16:44:04</p>
+                                    </div>
+                                    <div class="col-12 col-md-4">
+                                        <p class="text-bold-700 text-uppercase mb-0">IP</p>
+                                        <p class="mb-0">43.228.229.172</p>
+                                    </div>
+                                </div>
+                                <hr/>
+
+
+
+                                <form method="POST" class="form-horizontal form-user-profile row mt-2" action="../dao/servicoDao.php">
+                                    
+                                    <div class="col-6">
+                                        <fieldset class="form-label-group">
+                                            <input type="text" class="form-control" id="id" value="" readonly="" autofocus="" name="id">
+                                            <label for="id">Id</label>
+                                        </fieldset>
+                                    </div>
+
+                                 
+                                    <div class="col-3">
+                                        <p>Ativo</p>
+                                    </div>
+                                    <div class="col-3">
+                                        <input type="checkbox" id="switcherySize2" class="switchery" data-size="sm" checked name="ativo" />
+                                    </div>
+                                
+
+
+                                    <div class="col-6">
+                                        <fieldset class="form-label-group">
+                                            <input type="text" class="form-control" id="nome" value="" required="" autofocus="" name="nome">
+                                            <label for="nome">Nome</label>
+                                        </fieldset>
+                                    </div>
+  
+                                    <div class="col-6">
+                                        <fieldset class="form-label-group">
+                                            <input type="text" class="form-control" id="modalidade" value="" required="" autofocus="" name="modalidade">
+                                            <label for="modalidade">modalidade</label>
+                                        </fieldset>
+                                    </div>
+    
+
+ 
+                                    <div class="col-12 text-right">
+                                        <button type="submit" class="btn-gradient-primary my-1">Salvar</button>
+                                    </div>
+                                </form>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+<!--/ Recent Transactions -->
+<!-- Basic Horizontal Timeline -->
+
+<!--/ Basic Horizontal Timeline -->
+        </div>
+      </div>
+    </div>
+    <!-- ////////////////////////////////////////////////////////////////////////////-->
+
+
+    <footer class="footer footer-static footer-transparent">
+      <p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2"><span class="float-md-left d-block d-md-inline-block">Copyright  &copy; <script>
+          document.write(new Date().getFullYear())
+          </script> <a class="text-bold-800 grey darken-2" href="https://themeselection.com/" target="_blank">ThemeSelection </a>, All rights reserved. </span><span class="float-md-right d-block d-md-inline-blockd-none d-lg-block">Hand-crafted & Made with <i class="ft-heart pink"></i></span></p>
+    </footer>
+
+    <!-- BEGIN VENDOR JS-->
+    <script src="../vendors/js/vendors.min.js" type="text/javascript"></script>
+    <!-- BEGIN VENDOR JS-->
+       <script src="../vendors/js/forms/toggle/switchery.min.js" type="text/javascript"></script>
+    <!-- BEGIN PAGE VENDOR JS-->
+    <script src="../vendors/js/charts/chartist.min.js" type="text/javascript"></script>
+    <script src="../vendors/js/charts/chartist-plugin-tooltip.min.js" type="text/javascript"></script>
+    <script src="../vendors/js/timeline/horizontal-timeline.js" type="text/javascript"></script>
+    <!-- END PAGE VENDOR JS-->
+    <!-- BEGIN MODERN JS-->
+    <script src="../js/core/app-menu.js" type="text/javascript"></script>
+    <script src="../js/core/app.js" type="text/javascript"></script>
+    <!-- END MODERN JS-->
+    <!-- BEGIN PAGE LEVEL JS-->
+    <script src="../js/scripts/pages/dashboard-ico.js" type="text/javascript"></script>
+       <script src="../js/scripts/forms/account-profile.js" type="text/javascript"></script>
+    <!-- END PAGE LEVEL JS-->
+  </body>
+</html>
---
+++ consultass.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="CryptoDash admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities with bitcoin dashboard.">
+    <meta name="keywords" content="admin template, CryptoDash Cryptocurrency Dashboard Template, dashboard template, flat admin template, responsive admin template, web app, crypto dashboard, bitcoin dashboard">
+    <meta name="author" content="ThemeSelection">
+    <title>ICO Dashboard - CryptoDash - Free Cryptocurrency Dashboard Template + Bitcoin Dashboard</title>
+    <link rel="apple-touch-icon" href="../images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../images/ico/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,700,700i|Comfortaa:300,400,500,700" rel="stylesheet">
+    <!-- BEGIN VENDOR CSS-->
+    <link rel="stylesheet" type="text/css" href="../css/vendors.css">
+    <link rel="stylesheet" type="text/css" href="../vendors/css/charts/chartist.css">
+    <link rel="stylesheet" type="text/css" href="../vendors/css/charts/chartist-plugin-tooltip.css">
+    <!-- END VENDOR CSS-->
+    <!-- BEGIN MODERN CSS-->
+    <link rel="stylesheet" type="text/css" href="../css/app.css">
+    <!-- END MODERN CSS-->
+    <!-- BEGIN Page Level CSS-->
+    <link rel="stylesheet" type="text/css" href="../css/core/menu/menu-types/vertical-compact-menu.css">
+    <link rel="stylesheet" type="text/css" href="../vendors/css/cryptocoins/cryptocoins.css">
+    <link rel="stylesheet" type="text/css" href="../css/pages/timeline.css">
+    <link rel="stylesheet" type="text/css" href="../css/pages/dashboard-ico.css">
+    <!-- END Page Level CSS-->
+    <!-- BEGIN Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+    <!-- END Custom CSS-->
+  </head>
+  <body class="vertical-layout vertical-compact-menu 2-columns   menu-expanded fixed-navbar" data-open="click" data-menu="vertical-compact-menu" data-col="2-columns">
+
+    <!-- fixed-top-->
+    <nav class="header-navbar navbar-expand-md navbar navbar-with-menu navbar-without-dd-arrow fixed-top navbar-light navbar-bg-color">
+      <div class="navbar-wrapper">
+        <div class="navbar-header d-md-none">
+          <ul class="nav navbar-nav flex-row">
+            <li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu font-large-1"></i></a></li>
+            <li class="nav-item d-md-none"><a class="navbar-brand" href="index.php"><img class="brand-logo d-none d-md-block" alt="CryptoDash admin logo" src="../images/logo/logo.png"><img class="brand-logo d-sm-block d-md-none" alt="CryptoDash admin logo sm" src="../images/logo/logo-sm.png"></a></li>
+            <li class="nav-item d-md-none"><a class="nav-link open-navbar-container" data-toggle="collapse" data-target="#navbar-mobile"><i class="la la-ellipsis-v">   </i></a></li>
+          </ul>
+        </div>
+        <div class="navbar-container">
+          <div class="collapse navbar-collapse" id="navbar-mobile">
+            <ul class="nav navbar-nav mr-auto float-left">
+              <li class="nav-item d-none d-md-block"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu">         </i></a></li>
+              <li class="nav-item nav-search"><a class="nav-link nav-link-search" href="#"><i class="ficon ft-search"></i></a>
+                <div class="search-input">
+                  <input class="input" type="text" placeholder="Pesquisar...">
+                </div>
+              </li>
+            </ul>
+            <ul class="nav navbar-nav float-right">         
+   
+              <li class="dropdown dropdown-notification nav-item"><a class="nav-link nav-link-label" href="wallet.html"><i class="ficon icon-wallet"></i></a></li>
+              <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">             <span class="avatar avatar-online"><img src="../images/portrait/small/avatar-s-1.png" alt="avatar"></span><span class="mr-1">CIC<span class="user-name text-bold-700">3,458.88</span></span></a>
+                <div class="dropdown-menu dropdown-menu-right">             <a class="dropdown-item" href="account-profile.html"><i class="ft-award"></i>John Doe</a>
+                  <div class="dropdown-divider"></div><a class="dropdown-item" href="account-profile.html"><i class="ft-user"></i> Profile</a><a class="dropdown-item" href="wallet.html"><i class="icon-wallet"></i> My Wallet</a><a class="dropdown-item" href="transactions.html"><i class="ft-check-square"></i> Transactions              </a>
+                  <div class="dropdown-divider"></div><a class="dropdown-item" href="account-login.html"><i class="ft-power"></i> Logout</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <!-- ////////////////////////////////////////////////////////////////////////////-->
+
+
+    <div class="main-menu menu-fixed menu-dark menu-bg-default rounded menu-accordion menu-shadow">
+      <div class="main-menu-content"><a class="navigation-brand d-none d-md-block d-lg-block d-xl-block" href="index.php"><img class="brand-logo" alt="CryptoDash admin logo" src="../images/logo/logo.png"/></a>
+        <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+          <li class="nav-item"><a href="index.php"><i class="icon-grid"></i><span class="menu-title" data-i18n="">Dashboard</span></a>
+          </li>
+          <li class="active"><a href="consultass.php"><i class="icon-support"></i><span class="menu-title" data-i18n="">Consultas</span></a>
+          </li>
+          <li class=" nav-item"><a href="pacientess.php"><i class="icon-user-following"></i><span class="menu-title" data-i18n="">Pacientes</span></a>
+          </li>
+          <li class=" nav-item"><a href="psicologoss.php"><i class="icon-user"></i><span class="menu-title" data-i18n="">Psicologos</span></a>
+          </li>
+                                        <li class="active"><a href="servicos.php"><i class="la la-dashcube"></i><span class="menu-title" data-i18n="">Serviços</span></a>
+          </li>
+
+          <li class=" nav-item"><a href="usuarios.php"><i class="la la-users"></i><span class="menu-title" data-i18n="">Usuarios</span></a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+
+    <div class="app-content content">
+      <div class="content-wrapper">
+        <div class="content-header row">
+
+        <div class="content-header-left col-md-8 col-12 mb-2 breadcrumb-new">
+            <h3 class="content-header-title mb-0 d-inline-block">Consultas</h3>
+            <div class="row breadcrumbs-top d-inline-block">
+              <div class="breadcrumb-wrapper col-12">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="index.php">Dashboard</a>
+                  </li>
+                  <li class="breadcrumb-item active">Consultas
+                  </li>
+                </ol>
+              </div>
+            </div>
+          </div>
+
+          <div class="content-header-right col-md-4 col-12 d-none d-md-inline-block">
+            <div class="btn-group float-md-right"><a class="btn-gradient-secondary btn-sm white" href="addconsulta.php">Adicionar Consulta</a></div>
+          </div>
+        </div>
+        <div class="content-body"><!-- ICO Token &  Distribution-->
+
+
+<!--/ ICO Token balance & sale progress -->
+<!-- Recent Transactions -->
+<div class="row">
+    <div id="recent-transactions" class="col-12">
+        <h6 class="my-2">Consultas</h6>
+        <div class="card">
+            <div class="card-content">
+                <div class="table-responsive">
+                    <table id="recent-orders" class="table table-hover table-xl mb-0">
+                        <thead>
+                            <tr>
+                                <th class="border-top-0">Id</th>
+                                <th class="border-top-0">Ativo</th>
+                                <th class="border-top-0">Paciente</th>
+                                <th class="border-top-0">Psicologo</th>
+                                <th class="border-top-0">Data</th>
+                                <th class="border-top-0">Hora</th>
+                                <th class="border-top-0">Status</th>
+                                <th class="border-top-0">Data Registro</th>
+                                <th class="border-top-0">Ver</th>
+ 
+                            </tr>
+                        </thead>
+                        <tbody>
+
+
+                          <?php 
+//include "conexao.php";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname="agenda";
+
+// Create connection
+$conn = mysqli_connect($servername, $username);
+$banco = mysqli_select_db($conn,$dbname);
+mysqli_set_charset($conn,'utf8');
+// Check connection
+if (!$conn) {
+  die("Falha na Conexao: " . mysqli_connect_error());
+}
+
+$sql = mysqli_query($conn,"select * from consulta") or die("Erro");
+
+
+  while($dados = mysqli_fetch_assoc($sql)){
+  $id = $dados['id'];
+  $ativo = $dados['ativo'];
+  $paciente = $dados['paciente_id'];
+  $datan = $dados['dataconsulta'];
+  $hora = $dados['horaconsulta'];
+  $psicologo = $dados['psicologo_id'];
+  $status = $dados['status'];
+  $datareg = $dados['dataregistro']; ?>
+                            <tr>
+                                <td class="text-truncate"> <?php echo $id; ?></td>
+                  <td class="text-truncate"><a href="#"> <?php 
+if ($ativo = 1): ?>
+   <i class="la la-thumbs-up success float-right"></i>
+<?php else: ?>
+      <i class="la la-thumbs-up danger float-right"></i>
+
+<?php endif; ?></a> </td>
+                                <td class="text-truncate"><a href="#"><?php echo $paciente; ?></a><?php $paciente; ?></td>
+                                <td class="text-truncate">
+                                    <a href="#" ><?php echo $psicologo; ?></a>
+                                </td>
+                                <td class="text-truncate p-1"><?php echo $datan; ?></td>
+                                <td>
+                                    <?php echo $hora ?>
+                                </td>
+                                <td class="text-truncate"><?php echo $status; ?></td>
+                                <td><?php echo $datareg; ?></td>
+                                  <td><a href="#"><i class="la la-cog primary float-right"></i></a></td>
+                            </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/ Recent Transactions -->
+<!-- Basic Horizontal Timeline -->
+
+<!--/ Basic Horizontal Timeline -->
+        </div>
+      </div>
+    </div>
+    <!-- ////////////////////////////////////////////////////////////////////////////-->
+
+
+    <footer class="footer footer-static footer-transparent">
+      <p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2"><span class="float-md-left d-block d-md-inline-block">Copyright  &copy; <script>
+          document.write(new Date().getFullYear())
+          </script> <a class="text-bold-800 grey darken-2" href="https://themeselection.com/" target="_blank">ThemeSelection </a>, All rights reserved. </span><span class="float-md-right d-block d-md-inline-blockd-none d-lg-block">Hand-crafted & Made with <i class="ft-heart pink"></i></span></p>
+    </footer>
+
+    <!-- BEGIN VENDOR JS-->
+    <script src="../vendors/js/vendors.min.js" type="text/javascript"></script>
+    <!-- BEGIN VENDOR JS-->
+    <!-- BEGIN PAGE VENDOR JS-->
+    <script src="../vendors/js/charts/chartist.min.js" type="text/javascript"></script>
+    <script src="../vendors/js/charts/chartist-plugin-tooltip.min.js" type="text/javascript"></script>
+    <script src="../vendors/js/timeline/horizontal-timeline.js" type="text/javascript"></script>
+    <!-- END PAGE VENDOR JS-->
+    <!-- BEGIN MODERN JS-->
+    <script src="../js/core/app-menu.js" type="text/javascript"></script>
+    <script src="../js/core/app.js" type="text/javascript"></script>
+    <!-- END MODERN JS-->
+    <!-- BEGIN PAGE LEVEL JS-->
+    <script src="../js/scripts/pages/dashboard-ico.js" type="text/javascript"></script>
+    <!-- END PAGE LEVEL JS-->
+  </body>
+</html>
